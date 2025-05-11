c80196d2dc3d669221365e6d4c6b27f966bf39a9
Gia <miminoshvili2102@example.com>
2025-05-11 18:36:54 +0400

    now is no any error on vercel

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -21,7 +21,7 @@ export default function NotFound() {
             <li><span>throw new</span> <span>Error</span>{"("}<span>&quot;404: PageNotFoundError 😢&quot;</span>{");"}</li>
             <li>{"}"}</li>
             <li><br /></li>
-            <li>/* Suggestions:</li>
+            <li>{"/"}* Suggestions:</li>
             <li> * - Check the URL for typos</li>
             <li> * - Use the site navigation</li>
             <li> * - Or hit CMD+Z in real life 😅</li>
