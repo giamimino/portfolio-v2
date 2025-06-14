76cb965e2febae8fba7c6fcc0cf41dab4dcd8882
Gia <miminoshvili2102@example.com>
2025-06-14 18:48:02 +0400

    contact us page done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,14 @@
 import Header from "../../components/Header";
 import styles from "../page.module.scss";
 import Footer from "../../components/footer";
-import ContactC from "../../components/contactForm";
+import { ContactUs } from "../../components/contactForm";
 
 
 export default function Contacts() {
   return (
     <div className={styles.page}>
       <Header />
-      <ContactC />
+      <ContactUs />
       <Footer />
     </div>
   );
--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -1,12 +1,67 @@
 'use client'
 
 import styles from '../styles/contact.module.scss'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
-export default function ContactC() {
+export const ContactUs = () => {
+  const form = useRef<HTMLFormElement>(null)
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+        form.current!,
+        {
+          publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!,
+        }
+      )
+      .then(
+        () => {
+          console.log('SUCCESS!')
+          alert('Message sent successfully.')
+          form.current?.reset()
+        },
+        (error) => {
+          console.log('FAILED...', error.text)
+          alert('Failed to send message.')
+        }
+      )
+  }
 
   return (
     <main className={styles.contactWrapper}>
-      <h1 style={{color: "#fff"}}>Still in devlopment</h1>
+      <form ref={form} onSubmit={sendEmail} className={styles.contact}>
+        <div>
+          <label>Name</label>
+          <input type="text" name="name" required />
+        </div>
+
+        <div>
+          <label>Phone</label>
+          <input type="text" name="number" required />
+        </div>
+
+        <div>
+          <label>Email</label>
+          <input type="email" name="gmail" required />
+        </div>
+
+        <div>
+          <label>Title</label>
+          <input type="text" name="title" required />
+        </div>
+
+        <div>
+          <label>Message</label>
+          <textarea name="message" rows={8} cols={30} required />
+        </div>
+
+        <input type="submit" value="Send" />
+    </form>
     </main>
   )
 }
